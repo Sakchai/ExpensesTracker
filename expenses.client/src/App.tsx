c28be51b4f76371feea7b3cf0bcfdabe4333e3ca
Sakchai Thanasamut <sakchai.t@qnet.net>
2024-05-11 16:12:48 +0800

    update  expenses client

--- expenses.client/src/App.tsx
+++ expenses.client/src/App.tsx
@@ -1,56 +1,149 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+import { Route, Routes, useLocation } from 'react-router-dom';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Calendar from './pages/Calendar';
+import Chart from './pages/Chart';
+import ECommerce from './pages/Dashboard/ECommerce';
+import FormElements from './pages/Form/FormElements';
+import FormLayout from './pages/Form/FormLayout';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
+import Tables from './pages/Tables';
+import Alerts from './pages/UiElements/Alerts';
+import Buttons from './pages/UiElements/Buttons';
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+    const [loading, setLoading] = useState<boolean>(true);
+    const { pathname } = useLocation();
 
     useEffect(() => {
-        populateWeatherData();
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000);
     }, []);
 
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
-    return (
-        <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
-        </div>
+    return loading ? (
+        <Loader />
+    ) : (
+        <>
+            <Routes>
+                <Route
+                    index
+                    element={
+                        <>
+                            <PageTitle title="eCommerce Dashboard | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <ECommerce />
+                        </>
+                    }
+                />
+                <Route
+                    path="/calendar"
+                    element={
+                        <>
+                            <PageTitle title="Calendar | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Calendar />
+                        </>
+                    }
+                />
+                <Route
+                    path="/profile"
+                    element={
+                        <>
+                            <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Profile />
+                        </>
+                    }
+                />
+                <Route
+                    path="/forms/form-elements"
+                    element={
+                        <>
+                            <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <FormElements />
+                        </>
+                    }
+                />
+                <Route
+                    path="/forms/form-layout"
+                    element={
+                        <>
+                            <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <FormLayout />
+                        </>
+                    }
+                />
+                <Route
+                    path="/tables"
+                    element={
+                        <>
+                            <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Tables />
+                        </>
+                    }
+                />
+                <Route
+                    path="/settings"
+                    element={
+                        <>
+                            <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Settings />
+                        </>
+                    }
+                />
+                <Route
+                    path="/chart"
+                    element={
+                        <>
+                            <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Chart />
+                        </>
+                    }
+                />
+                <Route
+                    path="/ui/alerts"
+                    element={
+                        <>
+                            <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Alerts />
+                        </>
+                    }
+                />
+                <Route
+                    path="/ui/buttons"
+                    element={
+                        <>
+                            <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <Buttons />
+                        </>
+                    }
+                />
+                <Route
+                    path="/auth/signin"
+                    element={
+                        <>
+                            <PageTitle title="Signin | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <SignIn />
+                        </>
+                    }
+                />
+                <Route
+                    path="/auth/signup"
+                    element={
+                        <>
+                            <PageTitle title="Signup | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+                            <SignUp />
+                        </>
+                    }
+                />
+            </Routes>
+        </>
     );
-
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
 }
 
 export default App;
--- expenses.client/src/main.tsx
+++ expenses.client/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './css/style.css';
+import './css/satoshi.css';
+import 'jsvectormap/dist/css/jsvectormap.css';
+import 'flatpickr/dist/flatpickr.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>,
 )
